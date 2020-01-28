5a0aa16410c7e6d699fcdc3687e240893b586e28
Bryan-Huke <bhuke@outlook.com>
2020-01-28 14:40:27 -0700

    foundation for check checking written in black bishop

--- src/BlackBishop.java
+++ src/BlackBishop.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 
 @SuppressWarnings("rawtypes")
-public class BlackBishop extends Piece{
+public class BlackBishop extends Piece {
 
 	public BlackBishop(int x, int y) {
 		super(x, y);
@@ -10,6 +10,7 @@ public class BlackBishop extends Piece{
 
 	/**
 	 * Returns the current possible moves for this piece
+	 * 
 	 * @return ArrayList<Position> the ArrayList of all possible positions
 	 * @param b the board of the piece
 	 */
@@ -17,56 +18,65 @@ public class BlackBishop extends Piece{
 		ArrayList<Position> positions = new ArrayList<Position>();
 		Position p = new Position(this.getPos().getX(), this.getPos().getY());
 		int max = 1;
-		
-		//Positions to the diagonal up - right
-		do{
+
+		// Positions to the diagonal up - right
+		do {
 			p.setX(p.getX() + 1);
 			p.setY(p.getY() + 1);
-			max ++;
-		}while(b.isEmpty(p) && b.isValid(p) && !b.hasBlack(p));
-		for(int i = 0; i < max; i ++)
+			max++;
+		} while (b.isEmpty(p) && b.isValid(p) && !b.hasBlack(p));
+		for (int i = 0; i < max; i++)
 			positions.add(new Position(this.getPos().getX() + i, this.getPos().getY() + i));
-		
+
 		p.setY(this.getPos().getY());
 		p.setX(this.getPos().getX());
 		max = 1;
-		
-		//Positions to the diagonal down - left
-		do{
+
+		// Positions to the diagonal down - left
+		do {
 			p.setX(p.getX() - 1);
 			p.setY(p.getY() - 1);
-			max ++;
-		}while(b.isEmpty(p) && b.isValid(p) && !b.hasBlack(p));
-		for(int i = max - 1; i >= 0; i --)
+			max++;
+		} while (b.isEmpty(p) && b.isValid(p) && !b.hasBlack(p));
+		for (int i = max - 1; i >= 0; i--)
 			positions.add(new Position(this.getPos().getX() - i, this.getPos().getY() - i));
-		
+
 		p.setY(this.getPos().getY());
 		p.setX(this.getPos().getX());
 		max = 1;
-		
-		//Positions to the diagonal up - left
-		do{
+
+		// Positions to the diagonal up - left
+		do {
 			p.setX(p.getX() + 1);
 			p.setY(p.getY() - 1);
 			max += 1;
-		}while(b.isEmpty(p) && b.isValid(p) && !b.hasBlack(p));
-		for(int i = 0; i < max; i ++)
+		} while (b.isEmpty(p) && b.isValid(p) && !b.hasBlack(p));
+		for (int i = 0; i < max; i++)
 			positions.add(new Position(this.getPos().getX() + i, this.getPos().getY() - i));
-		
+
 		p.setY(this.getPos().getY());
 		p.setX(this.getPos().getX());
 		max = 1;
-		
-		//Positions to the diagonal down - right
-		do{
+
+		// Positions to the diagonal down - right
+		do {
 			p.setX(p.getX() - 1);
 			p.setY(p.getY() + 1);
-			max ++;
-		}while(b.isEmpty(p) && b.isValid(p) && !b.hasBlack(p));
-		for(int i = 0; i < max; i ++)
+			max++;
+		} while (b.isEmpty(p) && b.isValid(p) && !b.hasBlack(p));
+		for (int i = 0; i < max; i++)
 			positions.add(new Position(this.getPos().getX() - i, this.getPos().getY() + i));
-		
+
+		for (int i = 0; i < positions.size() - 1; i++) {
+			if (b.isValid(positions.get(i))) {
+				if (b.get(positions.get(i)) instanceof WhiteKing) {
+					((WhiteKing) b.get(positions.get(i))).setCheck(true);
+				}
+			}
+
+		}
 		return positions;
+
 	}
 
 	@Override
@@ -79,6 +89,8 @@ public class BlackBishop extends Piece{
 		return true;
 	}
 
-	public String toString(){ return "BlackBishop.png"; }
-	
+	public String toString() {
+		return "BlackBishop.png";
+	}
+
 }
